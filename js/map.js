81f9e5cd92e117587ddc8d20c7cafacf7b11b4b6
postolenko <denis.postolenko@gmail.com>
2020-07-21 12:47:13 +0300

    Rusthot

--- js/map.js
+++ js/map.js
@@ -6,7 +6,7 @@ ymaps.ready(function () {
         searchControlProvider: 'yandex#search'
     });
 
-    myPlacemark1 = new ymaps.Placemark([53.902512, 27.561481], {
+    myPlacemark1 = new ymaps.Placemark([59.819481, 30.084848], {
         hintContent: ''
     }, {
         iconLayout: 'default#imageWithContent',
@@ -15,16 +15,16 @@ ymaps.ready(function () {
         iconImageOffset: [27, -28]
     });
 
-    myPlacemark2 = new ymaps.Placemark([55.183672, 30.204791], {
+    myPlacemark2 = new ymaps.Placemark([59.827130, 30.082396], {
         hintContent: ''
     }, {
         iconLayout: 'default#imageWithContent',
         iconImageHref: 'img/map_marker.png',
         iconImageSize: [27, 33],
          iconImageOffset: [27, -28]
-    });        
+    });
 
-    myPlacemark3 = new ymaps.Placemark([54.510741, 30.429586], {
+    myPlacemark3 = new ymaps.Placemark([59.826092, 30.076345], {
         hintContent: ''
     }, {
         iconLayout: 'default#imageWithContent',
