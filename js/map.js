3e1bc957119e38a0c983ea73ea8546063c8639b9
postolenko <denis.postolenko@gmail.com>
2020-07-21 13:11:33 +0300

    Rusthot

--- js/map.js
+++ js/map.js
@@ -1,7 +1,7 @@
 ymaps.ready(function () {
     var myMap = new ymaps.Map('map', {
         center: [59.823543, 30.078758],
-        zoom: 13
+        zoom: 14
     }, {
         searchControlProvider: 'yandex#search'
     });
@@ -10,27 +10,27 @@ ymaps.ready(function () {
         hintContent: ''
     }, {
         iconLayout: 'default#imageWithContent',
-        iconImageHref: 'img/map_marker.png',
-        iconImageSize: [27, 33],
-        iconImageOffset: [27, -28]
+        iconImageHref: 'img/yellow_marker.png',
+        iconImageSize: [39, 35],
+        iconImageOffset: [19, -17]
     });
 
     myPlacemark2 = new ymaps.Placemark([59.827130, 30.082396], {
         hintContent: ''
     }, {
         iconLayout: 'default#imageWithContent',
-        iconImageHref: 'img/map_marker.png',
-        iconImageSize: [27, 33],
-         iconImageOffset: [27, -28]
+        iconImageHref: 'img/green_marker.png',
+        iconImageSize: [39, 35],
+        iconImageOffset: [19, -17]
     });
 
     myPlacemark3 = new ymaps.Placemark([59.826092, 30.076345], {
         hintContent: ''
     }, {
         iconLayout: 'default#imageWithContent',
-        iconImageHref: 'img/map_marker.png',
-        iconImageSize: [27, 33],
-         iconImageOffset: [27, -28]
+        iconImageHref: 'img/yellow_marker.png',
+        iconImageSize: [39, 35],
+        iconImageOffset: [19, -17]
     });
 
     myMap.geoObjects
